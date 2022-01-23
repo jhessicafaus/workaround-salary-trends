933a121d498757d9e6a402a9f563b7282284122d
Jess <92649040+jhessicafaus@users.noreply.github.com>
2022-01-22 21:19:51 -0400

    Add files via upload

---
+++ modules/utilities.js
@@ -0,0 +1,11 @@
+// This file was created for separation of concerns.
+
+// Format numbers.
+export const formatNumber = number => {
+  let numString = String(Math.floor(number));
+  for (let i = numString.length - 3; i > 0; i -= 3) {
+    numString = numString.slice(0, i) + ',' + numString.slice(i);
+  }
+  return numString;
+}
+   
